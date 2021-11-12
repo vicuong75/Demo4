e703ca0892c3884a12d926d2ef735bceb66be989
cuongvvph11678 <cuongvvph11678@users.noreply.github.com>
2021-11-12 12:44:00 +0700

    AAA

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
+/**
+ * @format
+ */
 
-var express = require('express');
-var expressHbs = require('express-handlebars');
-var app = express();
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 
-app.listen(process.env.PORT || '3000');
-
-
-
-app.engine('handlebars', expressHbs({
-    layoutsDir: __dirname + '/views/layouts',
-    // layout cha mặc định
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
-
-app.get('/', function (request,
-                       response) {
-var name = 'VICUONG';
-var arr =['A','B','C'];
-    response.render('home',{
-        title :name,date:'7-5-2001',array:arr
-    });
-});
-
-app.get('/login', function (request, response) {
-});
+AppRegistry.registerComponent(appName, () => App);
